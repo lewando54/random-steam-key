2be2b1bcecc2259656dfa7cc168861c3135201a0
VB2007 <vagvolgyi.balazs.vb@gmail.com>
2023-11-20 22:44:41 +0100

    modifications

--- random-steam-key.py
+++ random-steam-key.py
@@ -1,49 +1,44 @@
 import random
-
-def genKey():
-    symbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-           'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U',
-           'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4',
-           '5', '6', '7', '8', '9']
-    x = 0
-    newKey = ""
-    while x < 17:
-        digit = random.randint(0, 34)
-        if x == 5 or x == 11:
-            newKey += '-'
-        else:
-            newKey += symbols[digit]
-        x += 1
-    return newKey
-
-def genUniqueKeyToFile():
-    key = genKey()
-
-    f = open("keys.txt", "a+")
-
-    count = 0
-    while True:
-        line = f.readline()
-
-        if line == key:
-            key = genKey
-        if not line or line != key:
-            print("Your new unique key: "+key)
-            f.write(key+'\n')
-            break
-        count += 1
-    
-    f.close()
-
-genUniqueKeyToFile()
+import string
 
 affirmatives = ["y", "Y", "yes", "Yes", "yeah", "Yeah", "yup", "Yup"]
 negatives = ["n", "N", "no", "No", "nope", "Nope", "nah", "Nah"]
+
+def genKey():
+    try:
+        keyAmount = int(input("Amount of keys [integer] (default: 1): "))
+    except:
+        keyAmount = int(1)
+
+    keys = []
+    for i in range(keyAmount):
+        chars = string.ascii_uppercase + string.digits
+        key = '-'.join(''.join(random.choice(chars) for _ in range(5)) for _ in range(3))
+        i += 1
+        keys.append(key)
+        print(key)
+
+    isSaved = str(input("Would you like to save the key(s) [yes/no] (default: no)? "))
+
+    if isSaved in affirmatives:
+        saveName = str(input("Where would you like to save them [filename] (default: keys.txt)? "))
+        if saveName == "":
+            saveName = str("keys")
+        with open(f"{saveName}.txt", 'w') as f:
+            for key in keys:
+                f.write(f"{key}\n")
+        print(f"Keys saved to {saveName}.txt")
+    elif isSaved in negatives:
+        print("Skipping the saving part.")
+
+genKey()
+
 while True:
-    again = input("Generate another key? (y/n): ")
-    if again in affirmatives:
-        genUniqueKeyToFile()
-    elif again in negatives:
+    genMore = str(input("Would you like to generate more keys [yes/no] (default: no)? "))
+
+    if genMore in affirmatives:
+        genKey()
+    elif genMore in negatives or genMore == "":
         print("Understandable, have a nice day.")
         break
     else:
